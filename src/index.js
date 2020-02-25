b64d5b924f459e371cce030d90eab0c4350ac8d5
Salavei Dzmitry <salavei.dv@gmail.com>
2020-02-25 23:59:04 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let s = String(n);
+  s = s.split('');
+  if (s[0] == '-') {
+      delete s[0];
+  }
+  s = s.reverse();
+  s = s.join('');
+  let sn = Number(s);
+  return sn;
 }
